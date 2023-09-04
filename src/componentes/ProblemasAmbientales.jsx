1914320a1f58c1c30b3a193fb0f597599c7a19fc
Santiago Kasses <101513911+santiagokasses@users.noreply.github.com>
2023-09-04 10:40:50 -0300

    Update ProblemasAmbientales.jsx

--- src/componentes/ProblemasAmbientales.jsx
+++ src/componentes/ProblemasAmbientales.jsx
@@ -1,10 +1,11 @@
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
 import './ProblemasAmbientales.css';
-import React, { useEffect, useRef,useState } from 'react';
+/*import React, { useEffect, useRef,useState } from 'react';
+
 
 function ProblemasAmbientales() {
-
+    const textRef = useRef(null);
     const [textPosition, setTextPosition] = useState({ x: 0, y: 0 });
 
     const generateRandomPosition = () => {
@@ -15,11 +16,37 @@ function ProblemasAmbientales() {
       const randomY = Math.random() * maxY;
   
       setTextPosition({ x: randomX, y: randomY });
+
+      
+
+      useEffect(() => {
+        const backgroundImage = new Image();
+        backgroundImage.src = 'https://i.pinimg.com/564x/d6/d6/0a/d6d60a11f7f10eba6276763f54342f3b.jpg'; // Cambia a la ruta de tu imagen de fondo
+        
+        backgroundImage.onload = function() {
+          const canvas = document.createElement('canvas');
+          canvas.width = backgroundImage.width;
+          canvas.height = backgroundImage.height;
+    
+          const context = canvas.getContext('2d');
+          context.drawImage(backgroundImage, 0, 0, backgroundImage.width, backgroundImage.height);
+    
+          const textPositionX = textRef.current.offsetLeft;
+          const textPositionY = textRef.current.offsetTop;
+          
+          const pixel = context.getImageData(textPositionX, textPositionY, 1, 1).data;
+    
+          const backgroundColor = `rgb(${pixel[0]}, ${pixel[1]}, ${pixel[2]})`;
+    
+          textRef.current.style.backgroundColor = backgroundColor; // Cambia el color de fondo del texto
+        };
+      }, []);
+
     };
   
     return (
       <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
-        <button onClick={generateRandomPosition}>Generar Texto Aleatorio</button>
+        
         <div
           style={{
             position: 'absolute',
@@ -38,47 +65,80 @@ function ProblemasAmbientales() {
     );
 
 
-    /*const textRef = useRef(null);
+ 
+}
 
-    useEffect(() => {
-      const backgroundImage = new Image();
-      backgroundImage.src = 'https://i.pinimg.com/564x/d6/d6/0a/d6d60a11f7f10eba6276763f54342f3b.jpg'; // Cambia a la ruta de tu imagen de fondo
-      
-      backgroundImage.onload = function() {
-        const canvas = document.createElement('canvas');
-        canvas.width = backgroundImage.width;
-        canvas.height = backgroundImage.height;
-  
-        const context = canvas.getContext('2d');
-        context.drawImage(backgroundImage, 0, 0, backgroundImage.width, backgroundImage.height);
-  
-        const textPositionX = textRef.current.offsetLeft;
-        const textPositionY = textRef.current.offsetTop;
-        
-        const pixel = context.getImageData(textPositionX, textPositionY, 1, 1).data;
-  
-        const backgroundColor = `rgb(${pixel[0]}, ${pixel[1]}, ${pixel[2]})`;
-  
-        textRef.current.style.backgroundColor = backgroundColor; // Cambia el color de fondo del texto
-      };
-    }, []);
-  
-    return (
+export default ProblemasAmbientales;*/
+
+
+import React, { useEffect, useRef, useState } from 'react';
+
+function ProblemasAmbientales() {
+  const textRef = useRef(null);
+  const [textPosition, setTextPosition] = useState({ x: 0, y: 0 });
+
+  const generateRandomPosition = () => {
+    const maxX = window.innerWidth - 200; // Máxima posición X considerando el ancho del texto
+    const maxY = window.innerHeight - 50; // Máxima posición Y considerando la altura del texto
+
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+
+    setTextPosition({ x: randomX, y: randomY });
+    //console.log(randomX, randomY);
+  };
+
+  const loadBackgroundColor = () => {
+    const backgroundImage = new Image();
+    backgroundImage.src = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2000 1500'%3E%3Cdefs%3E%3CradialGradient id='a' gradientUnits='objectBoundingBox'%3E%3Cstop offset='0' stop-color='%230300FC'/%3E%3Cstop offset='1' stop-color='%23EC5422'/%3E%3C/radialGradient%3E%3ClinearGradient id='b' gradientUnits='userSpaceOnUse' x1='0' y1='750' x2='1550' y2='750'%3E%3Cstop offset='0' stop-color='%23782a8f'/%3E%3Cstop offset='1' stop-color='%23EC5422'/%3E%3C/linearGradient%3E%3Cpath id='s' fill='url(%23b)' d='M1549.2 51.6c-5.4 99.1-20.2 197.6-44.2 293.6c-24.1 96-57.4 189.4-99.3 278.6c-41.9 89.2-92.4 174.1-150.3 253.3c-58 79.2-123.4 152.6-195.1 219c-71.7 66.4-149.6 125.8-232.2 177.2c-82.7 51.4-170.1 94.7-260.7 129.1c-90.6 34.4-184.4 60-279.5 76.3C192.6 1495 96.1 1502 0 1500c96.1-2.1 191.8-13.3 285.4-33.6c93.6-20.2 185-49.5 272.5-87.2c87.6-37.7 171.3-83.8 249.6-137.3c78.4-53.5 151.5-114.5 217.9-181.7c66.5-67.2 126.4-140.7 178.6-218.9c52.3-78.3 96.9-161.4 133-247.9c36.1-86.5 63.8-176.2 82.6-267.6c18.8-91.4 28.6-184.4 29.6-277.4c0.3-27.6 23.2-48.7 50.8-48.4s49.5 21.8 49.2 49.5c0 0.7 0 1.3-0.1 2L1549.2 51.6z'/%3E%3Cg id='g'%3E%3Cuse href='%23s' transform='scale(0.12) rotate(60)'/%3E%3Cuse href='%23s' transform='scale(0.2) rotate(10)'/%3E%3Cuse href='%23s' transform='scale(0.25) rotate(40)'/%3E%3Cuse href='%23s' transform='scale(0.3) rotate(-20)'/%3E%3Cuse href='%23s' transform='scale(0.4) rotate(-30)'/%3E%3Cuse href='%23s' transform='scale(0.5) rotate(20)'/%3E%3Cuse href='%23s' transform='scale(0.6) rotate(60)'/%3E%3Cuse href='%23s' transform='scale(0.7) rotate(10)'/%3E%3Cuse href='%23s' transform='scale(0.835) rotate(-40)'/%3E%3Cuse href='%23s' transform='scale(0.9) rotate(40)'/%3E%3Cuse href='%23s' transform='scale(1.05) rotate(25)'/%3E%3Cuse href='%23s' transform='scale(1.2) rotate(8)'/%3E%3Cuse href='%23s' transform='scale(1.333) rotate(-60)'/%3E%3Cuse href='%23s' transform='scale(1.45) rotate(-30)'/%3E%3Cuse href='%23s' transform='scale(1.6) rotate(10)'/%3E%3C/g%3E%3C/defs%3E%3Cg transform='rotate(0 0 0)'%3E%3Cg transform='rotate(0 0 0)'%3E%3Ccircle fill='url(%23a)' r='3000'/%3E%3Cg opacity='0.5'%3E%3Ccircle fill='url(%23a)' r='2000'/%3E%3Ccircle fill='url(%23a)' r='1800'/%3E%3Ccircle fill='url(%23a)' r='1700'/%3E%3Ccircle fill='url(%23a)' r='1651'/%3E%3Ccircle fill='url(%23a)' r='1450'/%3E%3Ccircle fill='url(%23a)' r='1250'/%3E%3Ccircle fill='url(%23a)' r='1175'/%3E%3Ccircle fill='url(%23a)' r='900'/%3E%3Ccircle fill='url(%23a)' r='750'/%3E%3Ccircle fill='url(%23a)' r='500'/%3E%3Ccircle fill='url(%23a)' r='380'/%3E%3Ccircle fill='url(%23a)' r='250'/%3E%3C/g%3E%3Cg transform='rotate(0 0 0)'%3E%3Cuse href='%23g' transform='rotate(10)'/%3E%3Cuse href='%23g' transform='rotate(120)'/%3E%3Cuse href='%23g' transform='rotate(240)'/%3E%3C/g%3E%3Ccircle fill-opacity='0.1' fill='url(%23a)' r='3000'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E";
+      // Cambia a la ruta de tu imagen de fondo
+
+    backgroundImage.onload = function () {
+      const canvas = document.createElement('canvas');
+      canvas.width = backgroundImage.width;
+      canvas.height = backgroundImage.height;
+
+      const context = canvas.getContext('2d');
+      context.drawImage(backgroundImage, 0, 0, backgroundImage.width, backgroundImage.height);
+
+      const textPositionX = textRef.current.offsetLeft;
+      const textPositionY = textRef.current.offsetTop;
+
+      //console.log(textPositionX, textPositionY);
+      const pixel = context.getImageData(textPositionX, textPositionY, 1, 1).data;
+
+      const backgroundColor = `rgb(${pixel[0]}, ${pixel[1]}, ${pixel[2]})`;
+
+      textRef.current.style.color = backgroundColor; // Cambia el color de fondo del texto
+    };
+  };
+
+  useEffect(() => {
+    generateRandomPosition();
+  }, []); // Generar una posición aleatoria cuando el componente se monta
+
+  useEffect(() => {
+    loadBackgroundColor();
+  }, [textPosition]);
+
+  return (
+    <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
       <div
         ref={textRef}
         style={{
-          fontSize: '24px',
           position: 'absolute',
-          left: '50%',
-          top: '50%',
-          transform: 'translate(-50%, -50%)',
+          left: textPosition.x + 'px',
+          top: textPosition.y + 'px',
+          color: 'blue',
+          padding: '10px',
+          fontSize: '15px',
         }}
+        onMouseEnter={generateRandomPosition}
       >
-        Texto en Color Automático
+        Texto en Posición Aleatoria
       </div>
-    );*/
+    </div>
+  );
 }
 
 export default ProblemasAmbientales;
-
-
